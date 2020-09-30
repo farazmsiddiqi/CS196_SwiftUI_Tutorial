14dc64761c4b341149c2e6e1cccc8eb83120c78a
Faraz Siddiqi <farazsiddiqi@farazs-mbp.lan>
2020-09-30 14:55:57 -0700

    All code for 'creating and combining views' part 1 of SwiftUI tutorial

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Faraz Siddiqi on 9/30/20.
+//  Copyright © 2020 Faraz Siddiqi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+        .padding()
+            
+        Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
