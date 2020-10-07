a56a4b7798c70dac1d2f2d5222e6da5020a0b0c2
Faraz Siddiqi <farazsiddiqi@farazs-mbp.lan>
2020-10-06 18:33:54 -0700

    completed 'Animating Views and Transitions' SwiftUI Tutorial part 4

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/BadgeBackground.swift
@@ -0,0 +1,63 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Faraz Siddiqi on 10/6/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                path.move(
+                    to: CGPoint(
+                        x: xOffset + width * 0.95,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+                
+                HexagonParameters.points.forEach {
+                    path.addLine(
+                        to: .init(
+                            x: xOffset + width * $0.useWidth.0 * $0.xFactors.0,
+                            y: height * $0.useHeight.0 * $0.yFactors.0
+                        )
+                    )
+                    
+                    path.addQuadCurve(
+                        to: .init(
+                            x: xOffset + width * $0.useWidth.1 * $0.xFactors.1,
+                            y: height * $0.useHeight.1 * $0.yFactors.1
+                        ),
+                        control: .init(
+                            x: xOffset + width * $0.useWidth.2 * $0.xFactors.2,
+                            y: height * $0.useHeight.2 * $0.yFactors.2
+                        )
+                    )
+                }
+            }
+            .fill(LinearGradient(
+                gradient: .init(colors: [Self.gradientStart, Self.gradientEnd]),
+                startPoint: .init(x: 0.5, y: 0),
+                endPoint: .init(x: 0.5, y: 0.6)
+            ))
+            .aspectRatio(1, contentMode: .fit)
+        }
+    }
+    static let gradientStart = Color(red: 200.0 / 255, green: 150.0 / 255, blue: 201.0 / 255)
+    static let gradientEnd = Color(red: 19.0 / 255, green: 57.0 / 255, blue: 130.0 / 255)
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Faraz Siddiqi on 10/6/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
